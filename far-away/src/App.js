bb153b078ab067da917f1fb5f62a70ed0a1013a3
Deep Pansara <pansaradeep@gmail.com>
2023-06-15 07:55:28 +0530

    upload

---
+++ far-away/src/App.js
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+const Logo = () => {
+  return <h1>🏖️ Far Away 🎒</h1>;
+};
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip ?</h3>
+    </div>
+  );
+};
+const PackingList = () => {
+  return <div className="list">LIST</div>;
+};
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>You have x items in your list and you already packed X (X%)</em>
+    </footer>
+  );
+};
+
+export default App;
