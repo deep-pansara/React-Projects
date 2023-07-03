2e03747ac98fd630d3f483e9b1123c70e3b10483
Deep Pansara <pansaradeep@gmail.com>
2023-07-03 12:18:29 +0530

    added custom props in star rating component

--- pop-movies/src/index.js
+++ pop-movies/src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return (
+    <div>
+      <StarRating color="purple" maxRating={10} MovieRating={setMovieRating} />
+      <p>This movie is Rated {movieRating}</p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating maxRating={5} message={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating maxRating={10} className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
